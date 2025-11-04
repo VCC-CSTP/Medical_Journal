393538cc85745c094d2d1fa70d93615d481d85d1
Spuninst <domainjunkie@gmail.com>
2025-11-04 11:26:09 -0800

    Installed Supabase for database and tested

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,32 @@
-//import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
+import { createClient } from "@supabase/supabase-js";
+
+const supabase = createClient(
+  import.meta.env.VITE_SUPABASE_URL,
+  import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY
+);
+
 
 function App() {
-  //const [count, setCount] = useState(0)
+ const [organizations, setOrganizations] = useState([]);
+ useEffect(() => {
+   getInstruments();
+ }, []);
+
+  async function getInstruments() {
+    const { data } = await supabase.from("organizations").select();
+    setOrganizations(data);
+  }
 
   return (
     <>
-      <div>
-        <h1 class="text-3xl font-bold underline"> Hello world! </h1>
-      </div>
+      <ul>
+        {organizations.map((organization) => (
+          <li key={organization.org_name}>{organization.org_name}</li>
+        ))}
+      </ul>
+
     </>
   );
 }
